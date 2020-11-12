19d646b9cff3d184d6bdf9820c6292eae61b3e20
TishchenkoSergey <tishchenko.seriy@gmail.com>
2020-11-12 12:50:45 +0200

    Updates

---
+++ precache-manifest.ab87a9f1e00e6462dee43c459c11b3e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa5c35aef13b6f99f1a1f95523c727b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5f9bd5aa866fad879ba",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "93dcf1e0cfcb9fc7ca5b",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "c5f9bd5aa866fad879ba",
+    "url": "./static/js/2.71f32f69.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.71f32f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93dcf1e0cfcb9fc7ca5b",
+    "url": "./static/js/main.bdb903da.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
